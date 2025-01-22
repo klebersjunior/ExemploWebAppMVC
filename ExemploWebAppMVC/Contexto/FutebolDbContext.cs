45ed71569f683dbd9c3f603913d3eb9d15d02aac
kleberaquino <kleberaquino@KLEBERAQUIN3A84>
2025-01-21 22:06:22 -0300

    Add project files.

---
+++ ExemploWebAppMVC/Contexto/FutebolDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ExemploWebAppMVC.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExemploWebAppMVC.Contexto
+{
+    public class FutebolDbContext :DbContext
+    {
+        public FutebolDbContext(DbContextOptions<FutebolDbContext> options) : base(options) { 
+
+        }
+
+        //Inserir as tabelas
+        public DbSet<Times> Times { get; set; }
+    }
+}
---
+++ ExemploWebAppMVC/Modelos/Times.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExemploWebAppMVC.Modelos
+{
+    [Table("Times")]
+    public class Times
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataFundacao { get; set; }
+
+        public string Estado { get; set; }
+
+        public string NomeEstadio { get; set; }
+    }
+}
